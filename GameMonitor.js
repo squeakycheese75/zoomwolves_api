209f2b7ade469ba706a7b2900b2f2bbd40f5e4ff
Jamie <james_wooltorton@hotmail.com>
2020-04-10 20:47:03 +0100

    Added event emitter to handle character generation

---
+++ GameMonitor.js
@@ -0,0 +1,5 @@
+const EventEmitter = require('events')
+
+class GameMonitor extends EventEmitter {}
+
+module.exports = GameMonitor
--- api/controllers/games.js
+++ api/controllers/games.js
@@ -4,36 +4,49 @@ const router = express.Router()
 
 const { uuid } = require('uuidv4')
 
+// let events = require('events')
+// var eventEmitter = new events.EventEmitter()
+
 function normalizeName(name) {
   return name.replace(/[ ]/g, '_').toLowerCase()
 }
 
 var registeredgames = []
 
-router
-  .route('/')
-  .get((req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.send(registeredgames)
-  })
-  .post((req, res) => {
-    // console.log('req body is', req.body)
-    const clientInfo = req.body
-    const keyName = normalizeName(clientInfo.name)
+module.exports = function (monitor) {
+  const gameMonitor = monitor
 
-    const id = uuid()
-    // const resval = `Welcome brave ${keyName}, your game id is: ${id}.   Pass the link onto your players /api/player/${id}   You are now the zoomwolves master.`
-    const resval = { id, keyName }
-    registeredgames.push({ id, owner: keyName, status: 'open', players: [] })
-    res.setHeader('Content-Type', 'application/json')
-    res.send(resval)
+  gameMonitor.on('newGameStarted', (id) => {
+    console.log('newGameStarted detected', id)
   })
 
-router.route('/:id').get((req, res) => {
-  // Get individual player details registered to gameId
-  const game = registeredgames.find((q) => q.id === req.params.id)
-  res.setHeader('Content-Type', 'application/json')
-  res.send(game)
-})
+  router
+    .route('/')
+    .get((req, res) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.send(registeredgames)
+    })
+    .post((req, res) => {
+      // console.log('req body is', req.body)
+      const clientInfo = req.body
+      const keyName = normalizeName(clientInfo.name)
 
-module.exports = router
+      const id = uuid()
+      // const resval = `Welcome brave ${keyName}, your game id is: ${id}.   Pass the link onto your players /api/player/${id}   You are now the zoomwolves master.`
+      const resval = { id, keyName }
+      registeredgames.push({ id, owner: keyName, status: 'open', players: [] })
+      console.log('publishing event')
+      gameMonitor.emit('newGameStarted', id)
+      res.setHeader('Content-Type', 'application/json')
+      res.send(resval)
+    })
+
+  router.route('/:id').get((req, res) => {
+    // Get individual player details registered to gameId
+    const game = registeredgames.find((q) => q.id === req.params.id)
+    res.setHeader('Content-Type', 'application/json')
+    res.send(game)
+  })
+
+  return router
+}
